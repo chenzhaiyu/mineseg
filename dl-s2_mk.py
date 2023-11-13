d8a9541ea3e9128941f59d4f63660775d60e3551
Matthias <matthias.kahl@tum.de>
2023-11-13 12:54:40 +0100

    s2 downloader update

--- dl-s2_mk.py
+++ dl-s2_mk.py
@@ -9,6 +9,7 @@
 
 
 import argparse
+import asyncio
 import csv
 import os
 import subprocess
@@ -18,6 +19,27 @@ import itertools
 import gzip
 import pandas
 import pdb
+import multiprocessing
+import concurrent.futures
+
+
+def dl_task(params):
+    # print("########  WORKER: ", concurrent.futures.thread_name())
+    filename = params["filename"]
+    hdfs_file_path = params["hdfs_file_path"]
+    hdfs_dir_path = params["hdfs_dir_path"]
+    baseurl = params["baseurl"]
+    outf = params["outf"]
+
+    if not os.path.isfile(hdfs_file_path) or os.path.isdir(hdfs_dir_path):
+        print(filename + " does not exist.")
+        subprocess.call(f"gsutil -m cp -r {baseurl} {outf}", shell=True)
+        # subprocess.Popen(f"gsutil -m cp -r {baseurl} {outf}", shell=True)
+        # print("Compressing folder " + filename)
+        # shutil.make_archive(os.path.join(outf, filename), 'zip', outf, filename)
+        # shutil.rmtree(os.path.join(outf, filename))
+    else:
+        print(filename + " already exists.")
 
 
 def is_point_in_bbox(bbox_small, bbox_big):
@@ -34,8 +56,8 @@ def is_point_in_bbox(bbox_small, bbox_big):
     """
     min_lon, min_lat, max_lon, max_lat = bbox_big
 
-    lon = bbox_small[0] # - (bbox_small[0] - bbox_small[2]) / 2
-    lat = bbox_small[1] # - (bbox_small[1] - bbox_small[3]) / 2
+    lon = bbox_small[0] - (bbox_small[0] - bbox_small[2]) / 2
+    lat = bbox_small[1] - (bbox_small[1] - bbox_small[3]) / 2
 
     if min_lon < lon < max_lon and min_lat < lat < max_lat:
         return True
@@ -52,73 +74,77 @@ def run_cmd(args_list):
     return s_return, s_output, s_err
 
 
-path_to_csv = './dl/index.csv.gz'
-
-url = 'https://storage.googleapis.com/gcp-public-data-sentinel-2/index.csv.gz'
-if os.path.exists(path_to_csv):
-    print("Index file exists!")
-else:
-    subprocess.call(["wget", "-P", "dl/", url])
-
-now = time.time()
-
-if os.stat(path_to_csv).st_mtime < now - 7 * 86400:
-    print("Index file is older than 7 days, updating it.")
-    os.remove(path_to_csv)
-    subprocess.call(["wget", "-P", "dl/", url])
-else:
-    print("Index file is up-to-date.")
-
-# Definition of input parameters
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--cloud", type=int, required=True, help="Maximum cloud cover")
-# parser.add_argument("-g", "--granule", type=str, required=False, help="Granule ID to download (5 characters, e.g. 31UFS,31UFT - multiple granules supported)")
-parser.add_argument("-b", "--bbox",  nargs=4, type=float, required=True, help="bbox lat lon (4 floats, e.g. 31.6355 30.6432 -9.6665 -8.8971")
-parser.add_argument("-y", "--year", type=str, required=True, help="Years to download (multiple years supported)")
-parser.add_argument("-d", "--dir", type=str, required=True, help="Output directory.")
-args = parser.parse_args()
-
-th = args.cloud
-yr = args.year
-outf = args.dir
-yr = yr.split(",")
-bb = args.bbox
-print(yr)
-
-
 #  minlat maxlat minlon maxlon
 # -17.296715 -28.81443843 -71.535610 -66.97079090 !!!
 
-with gzip.open(path_to_csv, 'rt') as f:  # 'rt' for reading as text
-    reader = csv.reader(f)
-    header = next(reader)
-    for row in reader:
-        # Lon_min, Lat_min, Lon_max, Lat_max
-        bbox = [float(row[11]), float(row[10]), float(row[12]), float(row[9])]
-        # print(bbox, row[3])
-        # print(" ", bb)
-        if is_point_in_bbox(bbox, bb):
-            granule = row[3]
-            date = row[4]
-            date = date[0:4]
-            quality = row[7]  # geometric_quality_flag
-            cloud = row[6]
-            cl = int(float(cloud))
-            baseurl = row[13]
-            filename = baseurl.rsplit('/', 1)[-1]
 
-            if cl <= th:
-                for a in list(yr):
-                    if a == date:
-                        hdfs_file_path = os.path.join(outf, filename + ".zip")
-                        hdfs_dir_path = os.path.join(outf, filename)
+def main():
+    path_to_csv = './dl/index.csv.gz'
 
-                        if not os.path.isfile(hdfs_file_path) or os.path.isdir(hdfs_dir_path):
-                            print(filename + " does not exist.")
-                            subprocess.call(f"gsutil -m cp -r {baseurl} {outf}", shell=True)
+    url = 'https://storage.googleapis.com/gcp-public-data-sentinel-2/index.csv.gz'
+    if os.path.exists(path_to_csv):
+        print("Index file exists!")
+    else:
+        subprocess.call(["wget", "-P", "dl/", url])
 
-                            print("Compressing folder " + filename)
-                            shutil.make_archive(os.path.join(outf, filename), 'zip', outf, filename)
-                            shutil.rmtree(os.path.join(outf, filename))
-                        else:
-                            print(filename + " already exists.")
+    now = time.time()
+
+    if os.stat(path_to_csv).st_mtime < now - 7 * 86400:
+        print("Index file is older than 7 days, updating it.")
+        os.remove(path_to_csv)
+        subprocess.call(["wget", "-P", "dl/", url])
+    else:
+        print("Index file is up-to-date.")
+
+    # Definition of input parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cloud", type=int, required=True, help="Maximum cloud cover")
+    # parser.add_argument("-g", "--granule", type=str, required=False, help="Granule ID to download (5 characters, e.g. 31UFS,31UFT - multiple granules supported)")
+    parser.add_argument("-b", "--bbox", nargs=4, type=float, required=True,
+                        help="bbox lat lon (4 floats, e.g. 31.6355 30.6432 -9.6665 -8.8971")
+    parser.add_argument("-y", "--year", type=str, required=True, help="Years to download (multiple years supported)")
+    parser.add_argument("-d", "--dir", type=str, required=True, help="Output directory.")
+    args = parser.parse_args()
+
+    max_cloud_cover = args.cloud
+    years = args.year.split(",")
+    out_folder = args.dir
+    roi_bbox = args.bbox
+
+    # download 8 images
+    # go through the csv file
+    with gzip.open(path_to_csv, 'rt') as f:  # 'rt' for reading as text
+        reader = csv.reader(f)
+        header = next(reader)
+
+        for row in reader:
+            # Lon_min, Lat_min, Lon_max, Lat_max
+            bbox = [float(row[11]), float(row[10]), float(row[12]), float(row[9])]
+
+            # check if the point is in the defined bounding box
+            if is_point_in_bbox(bbox, roi_bbox):
+                granule = row[3]
+                date = row[4][0:4]
+                quality = row[7]  # geometric_quality_flag
+                cloud_cover = int(float(row[6]))
+                baseurl = row[13]
+                filename = baseurl.rsplit('/', 1)[-1]
+
+                # if cloud cover is below or equal threshold
+                if cloud_cover <= max_cloud_cover:
+                    for year in list(years):
+                        if year == date:
+                            hdfs_file_path = os.path.join(out_folder, filename + ".zip")
+                            hdfs_dir_path = os.path.join(out_folder, filename)
+
+                            dl_params = {'hdfs_file_path': hdfs_file_path,
+                                         'hdfs_dir_path': hdfs_dir_path,
+                                         'filename': filename,
+                                         'baseurl': baseurl,
+                                         'outf': out_folder}
+
+                            dl_task(params=dl_params)
+
+
+if __name__ == "__main__":
+    main()
