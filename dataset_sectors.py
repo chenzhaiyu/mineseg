373bd1e68d993c83787e8596c59f19765701a99b
matthias <matthias.kahl@tum.de>
2024-10-18 14:38:51 +0200

    add sector prediction

---
+++ dataset_sectors.py
@@ -0,0 +1,48 @@
+import os
+
+import cv2
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+class MiningSectorDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, remapping):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.filenames = os.listdir(image_dir)[:]
+        self.remapping = remapping
+    
+    def __len__(self):
+        return len(self.filenames)
+    
+    def __getitem__(self, idx):
+        image_filename = self.filenames[idx]
+        image_path = os.path.join(self.image_dir, image_filename)
+        mask_path = os.path.join(self.mask_dir, image_filename)  # Adjust file extension if needed
+        
+        # read data
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(mask_path, 0)
+
+        # convert data
+        image = torch.from_numpy(image.transpose(2, 0, 1).astype('float32'))
+        mask = torch.from_numpy(mask.astype('int64')).unsqueeze(0)
+
+        # map index for mask
+        if self.remapping is not None:
+            remapped_mask = mask.clone()
+            for old_value, new_value in self.remapping.items():
+                remapped_mask[mask == int(old_value)] = new_value
+            mask = remapped_mask
+        return image, mask
+
+
+def load_data(batch_size, num_workers, image_dir, mask_dir, remapping=None):
+    """
+    Data loading.
+    """
+    dataset = MiningSectorDataset(image_dir, mask_dir, remapping)
+    print(f"Dataset size: {len(dataset)}")
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    return dataloader
---
+++ predict_sectors.py
@@ -0,0 +1,111 @@
+"""
+Prediction (visualization) for multi-class mining site segmentation.
+"""
+
+import os
+
+import hydra
+from omegaconf import DictConfig
+from tqdm import tqdm
+import numpy as np
+import torch
+import cv2
+from torch.nn import DataParallel
+import segmentation_models_pytorch as smp
+
+from utils import prepare_plot, set_seed, init_device
+
+
+@hydra.main(config_path='./conf', config_name='config', version_base='1.2')
+def predict(cfg: DictConfig):
+    """
+    Prediction with one single input.
+    """
+    # initialize device
+    device = init_device(cfg.use_cuda, cfg.gpu_ids)
+
+    # fix randomness
+    set_seed(cfg.seed)
+
+    # define model: Unet, UnetPlusPlus, FPN, DeepLabV3, DeepLabV3Plus
+    if cfg.model == 'unet':
+        _model = smp.Unet
+    elif cfg.model == 'unetplusplus':
+        _model = smp.UnetPlusPlus
+    elif cfg.model == 'fpn':
+        _model = smp.FPN
+    elif cfg.model == 'deeplabv3':
+        _model = smp.DeepLabV3
+    elif cfg.model == 'deeplabv3plus':
+        _model = smp.DeepLabV3Plus
+    else:
+        raise ValueError('unexpected model architecture')
+    model = _model(
+        encoder_name=cfg.encoder,             # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+        encoder_weights=cfg.encoder_weights,  # use `imagenet` pre-trained weights for encoder initialization
+        in_channels=cfg.in_channel,           # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+        classes=len(cfg.classes),             # model output channels (number of classes in your dataset)
+        activation=cfg.activation,            # activation function after the final convolution layer
+    )
+    # port model to GPUs
+    model = DataParallel(model, device_ids=cfg.gpu_ids)
+    model.to(device)
+
+    # load example image paths
+    filenames = os.listdir(cfg.test_image_dir)[:cfg.num_examples]
+
+    # load checkpoint
+    state = torch.load(cfg.checkpoint_path)
+    model.load_state_dict(state['state_dict'])
+
+    # start evaluation
+    print('start prediction...')
+
+    # iterate over the randomly selected test image paths
+    model.eval()
+    for filename in tqdm(filenames):
+        # load image
+        image_path = os.path.join(cfg.test_image_dir, filename)
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image.astype("float32")
+
+        # resize the image and make a copy of it for visualization
+        origin = image.copy().astype(np.uint8)
+
+        # find the filename and generate the path to ground truth mask
+        gt_path = os.path.join(cfg.test_mask_dir, filename)
+
+        # load the ground-truth segmentation mask in grayscale mode and resize it
+        gt_mask = cv2.imread(gt_path, 0)
+
+        # remap gt mask if needed
+        if cfg.remapping is not None:
+            remapped_mask = gt_mask.copy()
+            for old_value, new_value in cfg.remapping.items():
+                remapped_mask[gt_mask == int(old_value)] = new_value
+            gt_mask = remapped_mask
+
+        # make the channel axis to be the leading one, add a batch
+        # dimension, create a PyTorch tensor, and flash it to the current device
+        image = np.transpose(image, (2, 0, 1))
+        image = np.expand_dims(image, 0)
+        image = torch.from_numpy(image).to(device)
+
+        # make the prediction and convert the result to a NumPy array
+        with torch.no_grad():
+            pred_mask = model(image).squeeze()
+            pred_mask = torch.argmax(pred_mask, dim=0)
+            pred_mask = pred_mask.cpu().numpy()
+
+        # convert prediction to integers
+        pred_mask = pred_mask.astype(np.uint8)
+
+        # prepare a plot for visualization
+        if not os.path.exists(f'{cfg.result_dir}'):
+            os.makedirs(f'{cfg.result_dir}')
+        prepare_plot(os.path.join(cfg.result_dir, filename), origin, gt_mask, pred_mask, cfg.classes)
+
+
+if __name__ == '__main__':
+    predict()
